1bd2e1e9af4f00131f54823d13afe501c71dc4fe
Patrick Downie <patdownie07@gmail.com>
2023-06-15 20:49:02 +0000

    created sidebar and music video scroll

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import "./reset.css";
+import logo from "./images/logo-png.png";
+import MusicVideos from "./components/MusicVideos.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="sitecontainer">
+        <div id="navdiv">
+          <img src={logo} alt="VERY COOL FILM ON DRAUGHT LOGO" id="mainlogo" />
+          <ul>
+            <li>HOME</li>
+            <li>MUSIC VIDEOS</li>
+            <li>SHORT FILMS</li>
+            <li>PHOTOGRAPHY</li>
+            <li>PRINTS</li>
+            <li>MYSTERY SECTION</li>
+          </ul>
+        </div>
+        <div id="contentcontainer">
+          <MusicVideos />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MusicVideos.jsx
@@ -0,0 +1,127 @@
+import thumb1 from "../images/thumb1.png";
+import thumb2 from "../images/thumb2.png";
+import thumb3 from "../images/thumb3.png";
+import thumb4 from "../images/thumb4.png";
+import thumb5 from "../images/thumb5.png";
+import thumb6 from "../images/thumb6.png";
+import thumb7 from "../images/thumb7.png";
+import "../MusicVideos.css";
+
+function MusicVideos() {
+  return (
+    <div id="musicvideosdiv">
+      <div id="outerscroller">
+        <div className="exit">
+          <div></div>
+          <div></div>
+        </div>
+        <div className="entrance">
+          <div></div>
+          <div></div>
+        </div>
+        <div id="innerscroller">
+          <div className="videodiv">
+            <img src={thumb1} alt="1" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb2} alt="2" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb3} alt="3" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb4} alt="4" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb5} alt="1" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb6} alt="1" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb7} alt="1" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb1} alt="1" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb2} alt="1" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb3} alt="1" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb4} alt="1" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb5} alt="1" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb6} alt="1" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+          <div className="videodiv">
+            <img src={thumb7} alt="1" className="videothumbnail" />
+            <div className="videoinfocard">
+              <p>Info about this vid</p>
+              <a href="https://www.youtube.com/watch?v=rKI0rQZhTCc">watch</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MusicVideos;
